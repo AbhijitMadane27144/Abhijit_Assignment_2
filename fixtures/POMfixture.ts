93bec6915bff7d5561b2445f15a3e096db62cce7
Abhijit Hanmant <abhijit.hanmant@kanini.com>
2025-10-20 17:32:09 +0530

    updated

--- fixtures/POMfixture.ts
+++ fixtures/POMfixture.ts
@@ -1,13 +1,12 @@
 import { test as basetest } from "@playwright/test";
-import {AdminPage} from "../Page/AdminLogin"
-import {CoupenPage1} from "../Page/CoupenPage1"
+import {AdminPage} from "../Page/AdminLogin";
+import{SignUpPage} from "../Page/SignUpPage";
 import {AdminProductPage} from "../Page/CreateProductPage"
-import{SignUpPage} from "../Page/SignUpPage"
+
 
 
 type myPOMFixture ={
 AdminLogin:AdminPage
-coupenpage:CoupenPage1
 CreateProduct:AdminProductPage
 signupPage:SignUpPage
 }
@@ -16,11 +15,6 @@ export const test=basetest.extend<myPOMFixture>({
 AdminLogin: async({page},use)=>{
 const adminlogin=new AdminPage(page)
 await use(adminlogin)
-},
-
-coupenpage: async({page},use)=>{
-const coupenpage=new CoupenPage1(page)
-await use(coupenpage)
 },
 
 CreateProduct: async({page},use)=>{
